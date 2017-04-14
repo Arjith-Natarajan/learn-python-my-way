3b1999c098b7e7255339d169265e6e6cd7189348
Arjith-Natarajan <arjith496@gmail.com>
2017-04-14 22:40:58 +0530

    First Commit

---
+++ ex11.py
@@ -0,0 +1,8 @@
+# print "How old are you"
+age = int(raw_input("How old are you?"))
+
+print "What is your name?"
+name = raw_input()
+
+print "Your name is ", name ,
+print "You are %d years old" % age
