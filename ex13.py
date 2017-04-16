def8ffc106e4de4c5d76f56cbf30724d44b80ddc
Arjith-Natarajan <arjith496@gmail.com>
2017-04-16 09:47:44 +0530

    Study Drills for ex13

--- ex13.py
+++ ex13.py
@@ -3,9 +3,15 @@
 # Importing modules
 from sys import argv
 
+# Having lesser than 3 args gives Value Error
+# Something like NullPointerException
 script, first, second, third = argv
 
 print "You are running from ", script
-print "This is 1st arg : ", first
-print "This is 2nd arg : ", second
-print "This is the last argument that is passed: ", third
+print "Your first variable : ", first
+print "Your second variable : ", second
+print "Your third variable : ", third
+
+fourth = int(raw_input('How many more variables ?'))
+
+print "You have remaining ", fourth ,"variables"
