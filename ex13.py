5df5c155526a51c1a2f81695784b171d1ca4faa8
Arjith-Natarajan <arjith496@gmail.com>
2017-04-16 09:41:45 +0530

    Exercise 13: Basic tasks completed

--- ex13.py
+++ ex13.py
@@ -1,1 +1,11 @@
 # Trying out git merge
+
+# Importing modules
+from sys import argv
+
+script, first, second, third = argv
+
+print "You are running from ", script
+print "This is 1st arg : ", first
+print "This is 2nd arg : ", second
+print "This is the last argument that is passed: ", third
