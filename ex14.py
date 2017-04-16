75ec8ef873a68fd0832509dc766fe624a1cee24d
Arjith-Natarajan <arjith496@gmail.com>
2017-04-16 10:06:39 +0530

    Study Drills for ex14

--- ex14.py
+++ ex14.py
@@ -2,13 +2,16 @@
 
 from sys import argv
 
-script, user_name = argv
-prompt = '>'
+script, user_name, nick_name  = argv
+prompt = '\n^_^ ->'          # Changed prompt
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
+
+print "Can I also call you %s ?" % nick_name
+call = raw_input(prompt)
 
 print "Where do you live %s?" % user_name
 lives = raw_input(prompt)
