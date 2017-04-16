e028eaf451c9c878aeae4ec8d20bfef0a1d65d22
Arjith-Natarajan <arjith496@gmail.com>
2017-04-16 11:21:11 +0530

    Study Drills and extensions for ex17
    Made the file less verbose and lesser lines
    Concatenating the object creation as well as read/write operati$

--- ex17.py
+++ ex17.py
@@ -6,8 +6,7 @@ script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
 
-in_file = open(from_file)
-in_data = in_file.read()
+in_data = open(from_file).read()
 
 print "The input file is %d bytes long." % len(in_data)
 
@@ -15,10 +14,6 @@ print "Does the output file really exists?  %r" % exists(to_file)
 print "Ready , hit RETURN to continue, CTRL-C to abort"
 raw_input()
 
-out_file = open(to_file,'w')
-out_file.write(in_data)
+open(to_file,'w').write(in_data)
 
 print "Wrapping up and closing"
-
-out_file.close()
-in_file.close()
