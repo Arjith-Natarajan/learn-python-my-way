0f816b41624b8ca7ee24f3d8bc06a94d668b5797
Akshaya Natarajan <akshnat410@gmail.com>
2017-04-20 12:06:03 +0530

    Add files via upload

--- learn_python_basics/ex35.py
+++ learn_python_basics/ex35.py
@@ -1,78 +1,59 @@
-from sys import exit
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
 
 
-def gold_room():
-    print "This room is full of gold.  How much do you take?"
-
-    choice = raw_input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number.")
-
-    if how_much < 50:
-        print "Nice, you're not greedy, you win!"
-        exit(0)
-    else:
-        dead("You greedy bastard!")
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
 
 
-def bear_room():
-    print "There is a bear here."
-    print "The bear has a bunch of honey."
-    print "The fat bear is in front of another door."
-    print "How are you going to move the bear?"
-    bear_moved = False
-
-    while True:
-        choice = raw_input("> ")
-
-        if choice == "take honey":
-            dead("The bear looks at you then slaps your face off.")
-        elif choice == "taunt bear" and not bear_moved:
-            print "The bear has moved from the door. You can go through it now."
-            bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif choice == "open door" and bear_moved:
-            gold_room()
-        else:
-            print "I got no idea what that means."
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
 
 
-def cthulhu_room():
-    print "Here you see the great evil Cthulhu."
-    print "He, it, whatever stares at you and you go insane."
-    print "Do you flee for your life or eat your head?"
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
 
-    choice = raw_input("> ")
-
-    if "flee" in choice:
-        start()
-    elif "head" in choice:
-        dead("Well that was tasty!")
-    else:
-        cthulhu_room()
+print '-' * 10
+print "Michigan's abbreviation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['Florida']
 
 
-def dead(why):
-    print why, "Good job!"
-    exit(0)
+print '-' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: ", cities[states['Florida']]
 
 
-def start():
-    print "You are in a dark room."
-    print "There is a door to your right and left."
-    print "Which one do you take?"
-
-    choice = raw_input("> ")
-
-    if choice == "left":
-        bear_room()
-    elif choice == "right":
-        cthulhu_room()
-    else:
-        dead("You stumble around the room until you starve.")
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s is abbreviated %s" % (state, abbrev)
 
 
-start()
+print '-' * 10
+for abbrev, city in cities.items():
+    print "%s has the city %s" % (abbrev, city)
+
+
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+
+print '-' * 10
+
+state = states.get('Texas')
+
+if not state:
+    print "Sorry, no Texas."
+
+
+city = cities.get('TX', 'Does Not Exist')
+print "The city for the state 'TX' is: %s" % city
